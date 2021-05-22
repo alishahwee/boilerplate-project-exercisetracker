67d96219829af3fbba691dacce41221535ce05ef
alishahwee <alisha.hwee@outlook.com>
2021-05-21 23:06:50 -0500

    Config moongoose

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose');
 require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
