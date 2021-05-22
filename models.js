8dd5e278bd3456fb3b7ebfa209bddd812aac0e11
alishahwee <alisha.hwee@outlook.com>
2021-05-22 10:53:38 -0500

    Ignore version keys

--- models.js
+++ models.js
@@ -2,15 +2,21 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-  username: { type: String, required: true },
-});
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true },
+  },
+  { versionKey: false }
+);
 
-const exerciseSchema = new Schema({
-  description: { type: String, required: true },
-  duration: { type: Number, required: true },
-  date: { type: Date, default: Date.now },
-});
+const exerciseSchema = new Schema(
+  {
+    description: { type: String, required: true },
+    duration: { type: Number, required: true },
+    date: { type: Date, default: Date.now },
+  },
+  { versionKey: false }
+);
 
 const User = mongoose.model('User', userSchema);
 const Exercise = mongoose.model('Exercise', exerciseSchema);
