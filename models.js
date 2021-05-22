94885e46380128f4586a3372dc272b717fb35e2e
alishahwee <alisha.hwee@outlook.com>
2021-05-22 10:56:06 -0500

    Add user id field to exercise model

--- models.js
+++ models.js
@@ -11,6 +11,7 @@ const userSchema = new Schema(
 
 const exerciseSchema = new Schema(
   {
+    _userId: { type: Schema.Types.ObjectId, required: true },
     description: { type: String, required: true },
     duration: { type: Number, required: true },
     date: { type: Date, default: Date.now },
