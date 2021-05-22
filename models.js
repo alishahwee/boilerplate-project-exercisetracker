3239df3f559ff380fb1eabf2afc3fa5ccf0721ca
alishahwee <alisha.hwee@outlook.com>
2021-05-22 10:34:10 -0500

    Modify model to separate users and exercises

--- models.js
+++ models.js
@@ -2,18 +2,18 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const usersSchema = new Schema({
+const userSchema = new Schema({
   username: { type: String, required: true },
-  count: { type: Number, required: true, default: 0 },
-  log: [
-    {
-      description: String,
-      duration: Number,
-      date: Date,
-    },
-  ],
 });
 
-const User = mongoose.model('User', usersSchema);
+const exerciseSchema = new Schema({
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: { type: Date, default: Date.now },
+});
 
-module.exports = User;
+const User = mongoose.model('User', userSchema);
+const Exercise = mongoose.model('Exercise', exerciseSchema);
+
+exports.User = User;
+exports.Exercise = Exercise;
