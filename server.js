abfb388eb7ad905baaddf4a6021353860e5cf69f
alishahwee <alisha.hwee@outlook.com>
2021-05-23 18:39:03 -0500

    Refactor to accomodate new schema changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
-const User = require('./models');
+const { User, Exercise } = require('./models');
 require('dotenv').config();
 
 mongoose.connect(process.env.MONGO_URI, {
@@ -13,30 +13,22 @@ mongoose.connect(process.env.MONGO_URI, {
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 // GET/POST users
 app
   .route('/api/users')
-  .get((req, res, next) => {
-    User.find({})
-      .select('_id username count log')
-      .exec((err, users) => {
-        err ? next(err) : res.json(users);
-      });
+  .get((_req, res, next) => {
+    User.find({}, (err, users) => {
+      err ? next(err) : res.json(users);
+    });
   })
   .post((req, res, next) => {
     const newUser = new User({ username: req.body.username });
-    newUser.save((err) => {
-      err
-        ? next(err)
-        : User.find(newUser)
-            .select('_id username')
-            .exec((err, user) => {
-              err ? next(err) : res.json(user);
-            });
+    newUser.save((err, user) => {
+      err ? next(err) : res.json(user);
     });
   });
 
